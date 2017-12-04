512fa814222fb854c37bf40fa8e3a57fce393b7e
William Asman <wtasman@hotmail.com>
2017-12-04 14:04:48 -0800

    adding euclids_gcd

---
+++ ch2/euclid_gcd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int gcd(int u, int v)
+{
+	int t;
+	while (u > 0)
+	{
+		if (u < v)
+			{
+				t = u;
+				u = v;
+				v = t;
+			}
+		u = u - v;
+	}
+	return v;
+}
+
+main()
+{
+	int x;
+	int y;
+	while (scanf("%d %d", &x, &y) != EOF)
+		if (x > 0 && y > 0)
+			printf("%d %d %d\n", x, y, gcd(x, y));
+}
